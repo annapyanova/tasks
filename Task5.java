5794a60f23f2db9eac908ac6760d00373da3ebf0
annapyanova <anya.pyanova@gmail.com>
2022-11-20 22:59:24 +0300

    Added comments to Task5

--- Task5.java
+++ Task5.java
@@ -7,22 +7,22 @@ import java.util.Set;
 
 public class Task5 {
     public static void main(String[] args) {
-        int[] one = new  int[]{72, 33, -73, 84, -12, -3, 13, -13, -68};
-        String[] five = new String[]{"toe", "ocelot", "maniac"};
-        System.out.println(Arrays.toString(encrypt("Sunshine")));
-        System.out.println(decrypt(one));
-        System.out.println(canMove("Queen", "C4", "D6"));
-        System.out.println(canComplete("butl", "beautiful"));
-        System.out.println(sumDigProd(1, 2, 3, 4, 5, 6));
-        System.out.println(Arrays.toString(sameVowelGroup(five)));
-        System.out.println(validateCard(1234567890123452L));
-        System.out.println(numToEng(17));
-        System.out.println(getSha256Hash("password123"));
-        System.out.println(correctTitle("jOn SnoW, kINg IN thE noRth."));
-        System.out.println(hexLattice(19));
+        int[] one = new  int[]{72, 33, -73, 84, -12, -3, 13, -13, -68}; //массив для 1 задания
+        String[] five = new String[]{"toe", "ocelot", "maniac"}; //массив для 5 задания
+        System.out.println(Arrays.toString(encrypt("Sunshine"))); //1
+        System.out.println(decrypt(one)); //1
+        System.out.println(canMove("Queen", "C4", "D6")); //2
+        System.out.println(canComplete("butl", "beautiful")); //3
+        System.out.println(sumDigProd(1, 2, 3, 4, 5, 6)); //4
+        System.out.println(Arrays.toString(sameVowelGroup(five))); //5
+        System.out.println(validateCard(1234567890123452L)); //6
+        System.out.println(numToEng(909)); //7
+        System.out.println(getSha256Hash("password123")); //8
+        System.out.println(correctTitle("jOn SnoW, kINg IN thE noRth.")); //9
+        System.out.println(hexLattice(19)); //10
     }
 
-    public static int[] encrypt(String a) {
+    public static int[] encrypt(String a) { //код строки: а[0] - код символа, остальные эелементы - различия между символами
         int lSymb = 0;
         int[] ascii = new int[a.length()];
         for (int i=0; i<a.length(); i++) {
@@ -31,45 +31,7 @@ public class Task5 {
         }
         return ascii;
     }
-    public static boolean canMove(String figure, String from, String to) {
-        if (figure.equals("Pawn")) {
-            if (from.charAt(0) == to.charAt(0)) {
-                if (from.charAt(1) == '2' && to.charAt(1) == '4') return true;
-                if (from.charAt(1) == '5' && to.charAt(1) == '7') return true;
-                if (Math.abs((int)from.charAt(1) - (int)to.charAt(1)) == 1) return true;
-            }
-        }
-        if (figure.equals("Horse")) {
-            if (Math.abs((int)from.charAt(0) - (int)to.charAt(0)) == 2
-                        && Math.abs((int)from.charAt(1) - (int)to.charAt(1)) == 1)
-                    return true;
-
-            if (Math.abs((int)from.charAt(1) - (int)to.charAt(1)) == 2
-                    && Math.abs((int)from.charAt(0) - (int)to.charAt(0)) == 1)
-                return true;
-        }
-        if (figure.equals("Bishop")) {
-            if (Math.abs((int)from.charAt(0) - (int)to.charAt(0)) == Math.abs((int)from.charAt(1) - (int)to.charAt(1)))
-                return true;
-        }
-        if (figure.equals("Rook")) {
-            if (from.charAt(0) == to.charAt(0) || from.charAt(1) == to.charAt(1))
-                return true;
-        }
-        if (figure.equals("Queen")) {
-            if (Math.abs((int)from.charAt(0) - (int)to.charAt(0)) == Math.abs((int)from.charAt(1) - (int)to.charAt(1)))
-                return true;
-            if (from.charAt(0) == to.charAt(0) || from.charAt(1) == to.charAt(1))
-                return true;
-        }
-        if (figure.equals("King")) {
-            if (Math.abs((int)from.charAt(0) - (int)to.charAt(0)) <= 1
-                    && Math.abs((int)from.charAt(1) - (int)to.charAt(1)) <= 1)
-                return true;
-        }
-        return false;
-    }
-    public static String decrypt(int[] s) {
+    public static String decrypt(int[] s) { //декодированное сообщение (код - массив)
         char lAscii = 0;
         String symbols = "";
         for (int i = 0; i < s.length; i++) {
@@ -78,7 +40,46 @@ public class Task5 {
         }
         return symbols;
     }
-    public static boolean canComplete(String a, String b) {
+    public static boolean canMove(String figure, String from, String to) { //проверка хода шахматной фигуры из её положения в целевую позицию
+        if (figure.equals("Pawn")) { //пешка
+            if (from.charAt(0) == to.charAt(0)) {
+                if (from.charAt(1) == '2' && to.charAt(1) == '4') return true;
+                if (from.charAt(1) == '5' && to.charAt(1) == '7') return true;
+                if (Math.abs((int)from.charAt(1) - (int)to.charAt(1)) == 1) return true;
+            }
+        }
+        if (figure.equals("Horse")) { //конь
+            if (Math.abs((int)from.charAt(0) - (int)to.charAt(0)) == 2
+                        && Math.abs((int)from.charAt(1) - (int)to.charAt(1)) == 1)
+                    return true;
+
+            if (Math.abs((int)from.charAt(1) - (int)to.charAt(1)) == 2
+                    && Math.abs((int)from.charAt(0) - (int)to.charAt(0)) == 1)
+                return true;
+        }
+        if (figure.equals("Bishop")) { //слон
+            if (Math.abs((int)from.charAt(0) - (int)to.charAt(0)) == Math.abs((int)from.charAt(1) - (int)to.charAt(1)))
+                return true;
+        }
+        if (figure.equals("Rook")) { //ладья
+            if (from.charAt(0) == to.charAt(0) || from.charAt(1) == to.charAt(1))
+                return true;
+        }
+        if (figure.equals("Queen")) { //ферзь
+            if (Math.abs((int)from.charAt(0) - (int)to.charAt(0)) == Math.abs((int)from.charAt(1) - (int)to.charAt(1)))
+                return true;
+            if (from.charAt(0) == to.charAt(0) || from.charAt(1) == to.charAt(1))
+                return true;
+        }
+        if (figure.equals("King")) { //король
+            if (Math.abs((int)from.charAt(0) - (int)to.charAt(0)) <= 1
+                    && Math.abs((int)from.charAt(1) - (int)to.charAt(1)) <= 1)
+                return true;
+        }
+        return false;
+    }
+    
+    public static boolean canComplete(String a, String b) { //проверка, может ли слово а превратить в b
         int n = 0;
         for (int i=0; i<b.length(); i++) {
             if (a.charAt(n) == b.charAt(i)) n++;
@@ -86,7 +87,7 @@ public class Task5 {
         if (n == a.length()) return true;
         else return false;
     }
-    public static int sumDigProd(int... a) {
+    public static int sumDigProd(int... a) { //сложение чисел -> произведение их цифр, пока ответ != одной цифре
         int sum = 0;
         for (int i : a) {
             sum += i;
@@ -96,7 +97,7 @@ public class Task5 {
         }
         return (sum);
     }
-    public static int multiply(int a) {
+    public static int multiply(int a) { //произведение цифр числа
         int m = 1;
         while (a > 0) {
             m *= a%10;
@@ -104,7 +105,7 @@ public class Task5 {
         }
         return m;
     }
-    public static String[] sameVowelGroup(String[] words) {
+    public static String[] sameVowelGroup(String[] words) { //выбор слов с такими же гласными, что и в первом слове
         Set<Character> vow1 = new HashSet<>();
         String word1 = words[0].replaceAll("[qwrtpsdfghjklzxcvbnm]", "");
         for (char charIn1 : word1.toCharArray()) {
@@ -122,7 +123,7 @@ public class Task5 {
         }
         return res.toArray(new String[]{});
     }
-    public static boolean validateCard(Long a) {
+    public static boolean validateCard(Long a) { //проверка на действительный номер кредитной карты
         if (a<Math.pow(10, 14) || a>=Math.pow(10, 20)) return false;
         int checkDigit = (int)(a % 10);
         String num = String.valueOf(a/10);
@@ -137,48 +138,45 @@ public class Task5 {
         }
         return (10 - (sum%10)) == checkDigit;
     }
-    public static String numToEng(int num) {
+    public static String numToEng(int num) { //строковое представление натурального числа < 1000
         String[] ones = {"zero","one","two","three","four","five","six","seven",
-                "eight","nine"};
+                "eight","nine"}; //единицы
         String[] tens1 = {"ten","eleven","twelve","thirteen","fourteen","fifteen",
-                "sixteen","seventeen","eighteen","nineteen"};
+                "sixteen","seventeen","eighteen","nineteen"}; //числа 10-19
         String[] tens2 = {"","twenty","thirty","forty","fifty","sixty","seventy","eighty",
-                "ninety"};
+                "ninety"}; //десятки
         String strNum = Integer.toString(num);
 
         switch (strNum.length()) {
-            case 1:
-                return ones[Integer.parseInt(strNum)];
-            case 2:
-                return tens1[Integer.parseInt(strNum) - 10];
-            case 3:
-                StringBuilder sb = new StringBuilder();
-                int[] digits = new int[3];
-                int k=0;
-                while(num != 0) {
-                    digits[k] = num % 10;
-                    num /= 10;
-                    k++;
+            case 1: //для чисел 0-9
+                return ones[num];
+            case 2: //для числе 10-99
+                if (num >= 10 && num <= 19) return tens1[num - 10];
+                else return tens2[num/10 - 1] + " " + ones[num%10];
+            case 3: //для числе 100-999
+                String res = ones[num/100] + " hundred ";
+                strNum = strNum.substring(1);
+                if (strNum.charAt(0) == '0') {
+                    strNum = strNum.substring(1);
+                    res += ones[Integer.parseInt(strNum)%10];
                 }
-                sb.append(ones[digits[2]]).append(" hundred ");
-                if (digits[1] == 1) {
-                    int c = digits[1] + digits[2];
-                    sb.append(tens1[c]);
-                    return sb.toString();
+                else {
+                    if (Integer.parseInt(strNum) >= 10 && Integer.parseInt(strNum) <= 19) res += tens1[Integer.parseInt(strNum) - 10];
+                    else res += tens2[Integer.parseInt(strNum)/10 - 1] + " " + ones[Integer.parseInt(strNum)%10];
                 }
-                if (digits[1] > 1) sb.append(tens2[digits[1]-1]).append(" ");
-                if (digits[0] > 0) sb.append(ones[digits[0]]);
-                return sb.toString();
+                return res;
             default:
                 break;
         }
         return "";
     }
-    public static String getSha256Hash(String str) {
+    public static String getSha256Hash(String str) { //хеш SHA-256 для данной строки
         String res = "";
         try {
+            //шифр байтов в строку
             MessageDigest md = MessageDigest.getInstance("SHA-256");
             byte[] hash = md.digest(str.getBytes());
+            //перевод их в хеш
             for (int i = 0; i < hash.length; i++){
                 String hex = Integer.toHexString(0xff & hash[i]);
                 if (hex.length() == 1) res += 0;
@@ -189,7 +187,7 @@ public class Task5 {
         }
         return res;
     }
-    public static String correctTitle(String str) {
+    public static String correctTitle(String str) { //все слова с большой буквы, кроме служебных
         String s = str.toLowerCase();
         String[] words = s.split(" ");
         String res = "";
@@ -199,7 +197,7 @@ public class Task5 {
         }
         return res;
     }
-    public static String hexLattice(int n) {
+    public static String hexLattice(int n) { //вывод на экран гексагональной решетки для центрированного шестиугольного числа
         int i = 0; //номер центрированного числа по порядку (0,1,2...)
         boolean isHexNum = false;
         while (3*i*(i+1)+1 <= n){
